a33ca3b4b0979ee7c2bffc00c515571a8d016f27
jgallud <jgallud@gmail.com>
2018-09-20 17:42:20 +0200

    modelo v0.1

--- modelo.js
+++ modelo.js
@@ -2,35 +2,26 @@
 function Juego(){
 	this.nombre="nombre";
 	this.usuarios=[];
-	this.preguntas=[];
+	this.cuestionarios=[];
 	this.agregarUsuario=function(usr){
 		this.usuarios.push(usr);
 	}
-	this.agregarPregunta=function(pregunta){
-		this.preguntas.push(pregunta);
+	this.agregarCuestionario=function(cuest){
+		this.cuestionarios.push(cuest);
 	}
-	this.preguntasIniciales=function(){
-		var preg=new Pregunta("Capital de Francia")
-		preg.agregarRespuesta(new Respuesta("Madrid",false));
-		preg.agregarRespuesta(new Respuesta("Lisboa",false));
-		preg.agregarRespuesta(new Respuesta("Paris",true));
-		preg.agregarRespuesta(new Respuesta("Toledo",false));
-		this.agregarPregunta(preg);
-
-		var preg=new Pregunta("Capital de Italia");
-		preg.agregarRespuesta(new Respuesta("Roma",true));
-		preg.agregarRespuesta(new Respuesta("Lisboa",false));
-		preg.agregarRespuesta(new Respuesta("Londres",false));
-		preg.agregarRespuesta(new Respuesta("Praga",false));
-		this.agregarPregunta(preg);
-	}
-
-	this.preguntasIniciales();
 }
 
 function Usuario(nombre){
 	this.nombre=nombre;
 	this.puntos=0;
+}
+
+function Cuestionario(nombre){
+	this.nombre=nombre;
+	this.preguntas=[];
+	this.agregarPregunta=function(pregunta){
+		this.preguntas.push(pregunta);
+	}
 }
 
 function Pregunta(enunciado){
@@ -44,4 +35,23 @@ function Pregunta(enunciado){
 function Respuesta(texto,correcta){
 	this.texto=texto;
 	this.correcta=correcta;
+}
+
+function cuestionarioTest(){
+	var cues=new Cuestionario("Capitales");
+	var preg=new Pregunta("Capital de Francia")
+	preg.agregarRespuesta(new Respuesta("Madrid",false));
+	preg.agregarRespuesta(new Respuesta("Lisboa",false));
+	preg.agregarRespuesta(new Respuesta("Paris",true));
+	preg.agregarRespuesta(new Respuesta("Toledo",false));
+	cues.agregarPregunta(preg);
+
+	var preg=new Pregunta("Capital de Italia");
+	preg.agregarRespuesta(new Respuesta("Roma",true));
+	preg.agregarRespuesta(new Respuesta("Lisboa",false));
+	preg.agregarRespuesta(new Respuesta("Londres",false));
+	preg.agregarRespuesta(new Respuesta("Praga",false));
+	cues.agregarPregunta(preg);
+	
+	return cues;
 }
